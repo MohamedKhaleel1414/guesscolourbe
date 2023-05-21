ec04611ec51b04b08746b1a42bb4b2f9665f15f5
Mohamed Khaleel <113287240+MohamedKhaleel1414@users.noreply.github.com>
2023-05-21 05:53:16 +0300

    forget

--- controllers/forgetPassword.js
+++ controllers/forgetPassword.js
@@ -3,11 +3,13 @@ const { post } = require("..");
 const User = require("../models/users");
 const nodemailer = require("nodemailer");
 const axios = require("axios");
+const fs = require('fs')
 
 const forgetPass = async (req, res) => {
   if (req.body.email) {
     const userRequested = await User.findOne({ email: req.body.email }, {});
     if (userRequested) {
+      const htmlFile = fs.readFileSync('./views/forgetEmailContent.html',{encoding:"utf-8"})
       axios.default
         .post(
           "https://guesscolour.onrender.com/assignpassword/saveemail",
@@ -36,24 +38,7 @@ const forgetPass = async (req, res) => {
         from: process.env.EMAIL_USER,
         to: req.body.email,
         subject: "Assign your Password",
-        html: `<!DOCTYPE html>
-                <html lang="en">
-                  <head>
-                    <meta charset="UTF-8" />
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <title>Assign Password</title>
-                  </head>
-                  <body style="width: 100%;height: 50px;color:rgb(82, 81, 81);">
-                    <p style="text-align: center;">
-                      A request has been received to change the password for your Guess Colour
-                      account. 
-                    </p>
-                    <div style="text-align: center;">
-                        <a href='https://www.google.com'>CLick Here</a>
-                    </div>
-                  </body>
-                </html>`,
+        html: htmlFile,
       };
       transport.sendMail(message, (err, info) => {
         if (err) {
