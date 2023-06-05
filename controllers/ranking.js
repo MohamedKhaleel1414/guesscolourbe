a6529d0572aeac99eae180c778701b235dc7a097
Mohamed Khaleel <113287240+MohamedKhaleel1414@users.noreply.github.com>
2023-06-05 19:16:47 +0300

    points

--- controllers/ranking.js
+++ controllers/ranking.js
@@ -1,12 +1,45 @@
-const User = require('../models/users')
+const { map } = require("..");
+const User = require("../models/users");
 
-const ranking = async (req,res) => {
-    let list = await User.find({},{username:1,totalpoints:1,_id:0}).sort({totalpoints:-1}).limit(10)
-    if(list){
-        res.status(200).send(list)
+const ranking = async (req, res) => {
+  let list = await User.find({}, { username: 1, totalpoints: 1, _id: 0 })
+    .sort({ totalpoints: -1 })
+    .limit(10);
+  if (list) {
+    res.status(200).send(list);
+  } else {
+    res.status(400).send("Bad request. An error occured");
+  }
+};
+
+const yourRank = async (req, res) => {
+  let sentIndex = 0;
+  let list = await User.find({}, { username: 1, totalpoints: 1, _id: 0 }).sort({
+    totalpoints: -1,
+  });
+  if (list) {
+    let user = await User.findOne(
+      { username: req.body.username },
+      { username: 1, totalpoints: 1, _id: 0 }
+    );
+    if (user) {
+      await Promise.all(
+        list.map((usr, idx) => {
+          let stringusr = usr.toString();
+          let curruser = user.toString();
+          if (stringusr === curruser) sentIndex = idx;
+        })
+      );
+      let sentData = {
+        position:sentIndex
+      }
+      res.status(200).send(sentData);
     } else {
-        res.status(400).send("Bad request. An error occured")
+        res.status(404).send("User not found")
     }
-}
+  } else {
+    res.status(404).send("No data to show")
+  }
+};
 
-module.exports = {ranking}
+module.exports = { ranking, yourRank };
--- routes/rankingRouter.js
+++ routes/rankingRouter.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const rankingController = require('../controllers/ranking')
 
 router.get('/getranking',rankingController.ranking)
+router.get('/getyourrank',rankingController.yourRank)
 
 module.exports = router
